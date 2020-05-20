8a25fdd7713dedc610ed2cae14eecd20be86522e
Bayani <bayani.cruz@gmail.com>
2020-05-20 20:49:07 +1000

    Adding delete function

--- src/commands/dxb/source/delta.ts
+++ src/commands/dxb/source/delta.ts
@@ -18,7 +18,8 @@ export default class extends SfdxCommand {
     mode: flags.string({ char: 'm', description: 'commitid|tags|branch', default: "commitid" }),
     deltakey: flags.string({ char: 'k', description: 'commit id, tags prefix or name, branch name' }),
     basedir: flags.string({ char: 'd', description: 'path of base directory', default: 'force-app/main/default' }),
-    testclsnameregex: flags.string({ char: 'n', description: 'Regex for test classes naming convention', default: '.*Test' })
+    testclsnameregex: flags.string({ char: 'n', description: 'Regex for test classes naming convention', default: '.*Test' }),
+    filter: flags.string({ char: 'f', description: 'diff filter', default: 'AMR' })
   };
 
   // Comment this out if your command does not require an org username
@@ -37,10 +38,11 @@ export default class extends SfdxCommand {
   public async run() {
     let mode = this.flags.mode;
     let deltakey = this.flags.deltakey;
+    let filter = this.flags.filter;
     this.regex = this.flags.testclsnameregex;
     basedir = this.flags.basedir;
 
-    let deltaMeta = this.getDeltaChanges(mode, deltakey);
+    let deltaMeta = this.getDeltaChanges(mode, deltakey, filter);
     let deployOutput = '';
     if (deltaMeta && deltaMeta.length > 0) {
       deployOutput += `${deltaMeta.join(',')}`;
@@ -51,18 +53,18 @@ export default class extends SfdxCommand {
   private onlyUnique(value: any, index: any, self: any) {
     return self.indexOf(value) === index && value.startsWith(basedir);
   }
-  public getDeltaChanges(mode: any, deltakey: any): any {
+  public getDeltaChanges(mode: any, deltakey: any, filter: any): any {
     var gitresult;
     if (mode === 'branch') {
-      gitresult = exec(`git diff ${deltakey} --name-only --diff-filter=AMR`).toString().split('\n');
+      gitresult = exec(`git diff ${deltakey} --name-only --diff-filter=${filter}`).toString().split('\n');
     } else if (mode === 'tags') {
       if (deltakey) {
-        gitresult = exec(`git diff $(git describe --match ${deltakey}* --abbrev=0 --all)..HEAD --name-only --diff-filter=AMR`).toString().split('\n');
+        gitresult = exec(`git diff $(git describe --match ${deltakey}* --abbrev=0 --all)..HEAD --name-only --diff-filter=${filter}`).toString().split('\n');
       } else {
-        gitresult = exec(`git diff $(git describe --tags --abbrev=0 --all)..HEAD --name-only --diff-filter=AMR`).toString().split('\n');
+        gitresult = exec(`git diff $(git describe --tags --abbrev=0 --all)..HEAD --name-only --diff-filter=${filter}`).toString().split('\n');
       }
     } else {
-      gitresult = exec(`git diff-tree --no-commit-id --name-only -r ${deltakey} --diff-filter=AMR`).toString().split('\n'); //this only work with specific commit ids, how to get file that changed since last tag ? 
+      gitresult = exec(`git diff-tree --no-commit-id --name-only -r ${deltakey} --diff-filter=${filter}`).toString().split('\n'); //this only work with specific commit ids, how to get file that changed since last tag ? 
     }
     //filter unnecessary files
     var files = gitresult.filter(this.onlyUnique);
